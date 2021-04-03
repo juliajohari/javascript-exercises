a901a8f7e13d05cbe20b322b0d0642ceeb3c1fe2
Julia Johari <julia.johari@gmail.com>
2021-04-03 17:39:30 -0600

    findTheOldest commit

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
-
-}
+let findTheOldest = function(people) {
+    let oldest = people[0];
+      for (let i = 0; i < people.length; i++) {
+              if (!people[i].yearOfDeath) {
+                  people[i].age = 2021 - people[i].yearOfBirth
+              } else {
+                  people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+              }//end else
+              if (oldest.age < people[i].age) {
+                  oldest = people[i];
+              }//end if
+      }//end i loop
+      return oldest;
+  }//end findTheOldest
 
 module.exports = findTheOldest
