a3fce9bbe07db6a11ea7cafc3650ef68482064d0
Julia Johari <julia.johari@gmail.com>
2021-04-03 18:42:58 -0600

    Git Commit 3

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
-}
+const leapYears = function(year) {
+    let reply = false;
+    if (year % 4 === 0 && year % 100 != 0 || (year % 100 === 0 && year % 400 === 0)) {
+      reply = true;
+    }//end if
+    return reply;
+}//end leapYears
 
 module.exports = leapYears
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-const ftoc = function() {
+const ftoc = function(inputTemp) {
+  return parseFloat(((inputTemp - 32) * 5/9).toFixed(1));
+}//end ftoc
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function(inputTemp) {
+  return parseFloat(((inputTemp * 9/5) + 32).toFixed(1));
+}//end ctof
 
 module.exports = {
   ftoc,
